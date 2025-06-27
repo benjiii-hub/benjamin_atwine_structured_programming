7292f7a07e98d80c9e05c97a424ee299f4ee7127
benjiiii <kline9341@gmail.com>
2025-06-27 11:50:19 +0300

    Add files via upload

---
+++ new.py
@@ -0,0 +1,8 @@
+print("enter your user name")
+user_name = input()
+print("enter your password")
+password = input()
+if user_name == "Francis" and password == "123456789":
+    print("Authentication successful")
+else:
+    print("Authentication failed")
